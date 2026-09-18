0fe0314058c08d1185f22d0989d417530d0ffdc6
nosdvp <mishchenkoa.97@gmail.com>
2026-09-18 21:51:12 +0300

    Add game menu and rules modal Added game/menu header states Added rules modal Added modal close functionality Added rules image display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import rules from './img/image-rules.svg'
 
 function App() {
+
+  const [openModalRules, setOpenModalRules] = useState(false)
+  const [menu, setMenu] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+
+      {menu === true ? (
+        <>
+          <div className='wrapper__headerMenu'>
+            <p>ROCK</p>
+            <p>PAPER</p>
+            <p>SCISSORS</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className='wrapper__headerGame'>
+            <div className='wrapper__headerGame_title'>
+              <p>ROCK</p>
+              <p>PAPER</p>
+              <p>SCISSORS</p>
+            </div>
+            <div className='wrapper__headerGame_score'>
+              <p>SCORE</p>
+              <p>12</p>
+            </div>
+          </div>
+        </>
+      )}
+
+
+      <button className='wrapper__rules' onClick={() => setOpenModalRules(true)}>RULES</button>
+
+      {openModalRules === true && (
+        <div className='wrapper__modal'>
+          <div className='wrapper__modal_rulesBlock'>
+            <div className='wrapper__modal_rulesBlock_titleBlock'>
+              <p className='wrapper__modal_rulesBlock_titleBlock_title'>RULES</p>
+              <p className='wrapper__modal_rulesBlock_titleBlock_close' onClick={() => setOpenModalRules(false)}>X</p>
+            </div>
+
+            <div className='wrapper__modal_rulesBlock_rulesBox'>
+              <img src={rules}/>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
