50f5c77eeb6c611ed613e076752115a622542840
nosdvp <mishchenkoa.97@gmail.com>
2026-09-22 09:21:43 +0300

    added modal window after click on button Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { use, useState } from 'react';
 import './App.css';
 import rules from './img/image-rules.svg'
+import rulesModern from './img/image-rules-bonus.svg'
 import paper from './img/icon-paper.svg'
 import scissors from './img/icon-scissors.svg'
 import rock from './img/icon-rock.svg'
+import lizard from './img/icon-lizard.svg'
+import spock from './img/icon-spock.svg'
 
 function App() {
 
-  const [openModalRules, setOpenModalRules] = useState(false)
+  const [openModalRulesStandard, setOpenModalRulesStandard] = useState(false)
+  const [openModalRulesModern, setOpenModalRulesModern] = useState(false)
   const [menu, setMenu] = useState(true)
   const [mode, setMode] = useState('')
   const [userScore, setUserScore] = useState(0)
@@ -19,6 +23,7 @@ function App() {
   const [CPUItem, setCPUItem] = useState('')
   const [visibleCPUItem, setVisibleCPUItem] = useState()
   const [CPUImg, setCPUImg] = useState()
+  const [modalBeforeGoToMenu, setModalBeforeGoToMenu] = useState(false)
 
   const playGame = (userItem) => {
     setUserItem(userItem)
@@ -116,49 +121,48 @@ function App() {
 
           {userItem === '' ? (
             <div className='wrapper__headerGameStandard_choiseBlock'>
-            <button 
-              className='wrapper__headerGameStandard_choiseBlock_firstItem' 
-              onClick={() => playGame('paper')}
-            >
-              <img src={paper}/>
-            </button>
-            <button 
-              className='wrapper__headerGameStandard_choiseBlock_secondItem' 
-              onClick={() => playGame('scissors')}
+              <button 
+                className='wrapper__headerGameStandard_choiseBlock_firstItem' 
+                onClick={() => playGame('paper')}
               >
-                <img src={scissors}/>
-            </button>
-            <button 
-              className='wrapper__headerGameStandard_choiseBlock_thirdItem' 
-              onClick={() => playGame('rock')}
-            >
-              <img src={rock}/>
-            </button>
-          </div>
+                <img src={paper}/>
+              </button>
+              <button 
+                className='wrapper__headerGameStandard_choiseBlock_secondItem' 
+                onClick={() => playGame('scissors')}
+                >
+                  <img src={scissors}/>
+              </button>
+              <button 
+                className='wrapper__headerGameStandard_choiseBlock_thirdItem' 
+                onClick={() => playGame('rock')}
+              >
+                <img src={rock}/>
+              </button>
+            </div>
           ) : (
-              <div className='wrapper__headerGameStandard_blockResult'>
-
-                <div className='wrapper__headerGameStandard_blockResult_userItemBlock'>
-                  <p>YOU PICKED</p>
-                  <div className={visibleUserItem}>
-                    <img src={IMG}/>
-                  </div>
+              <>
+            <div className='wrapper__headerGameStandard_blockResult'>
+              <div className='wrapper__headerGameStandard_blockResult_userItemBlock'>
+                <p>YOU PICKED</p>
+                <div className={visibleUserItem}>
+                  <img src={IMG}/>
                 </div>
-
-                <div className='wrapper__headerGameStandard_blockResult_CPUItemBlock'>
-                  <p>THE HOUSE PICKED</p>
-                  <div className={visibleCPUItem}>
-                    <img src={CPUImg}/>
-                  </div>
-                </div>
-
-                
-                <button className='restart' onClick={() => setUserItem('')}>Restart</button>
               </div>
+              <div className='wrapper__headerGameStandard_blockResult_CPUItemBlock'>
+                <p>THE HOUSE PICKED</p>
+                <div className={visibleCPUItem}>
+                  <img src={CPUImg}/>
+                </div>
+              </div>
+              <button className='restart' onClick={() => setUserItem('')}>Restart</button>
+            </div>
+              </>
           )}
         </>
       ) : menu === false && mode === 'modern' ? (
-        <div className='wrapper__headerGameModern'>
+        <>
+          <div className='wrapper__headerGameModern'>
             <div className='wrapper__headerGameModern_title'>
               <p>ROCK</p>
               <p>PAPER</p>
@@ -173,26 +177,113 @@ function App() {
               <p>CPU Win: {CPUScore}</p>
             </div>
           </div>
+
+          {userItem === '' ? (
+            <div className='wrapper__headerGameModern_choiseBlockModern'>
+
+              <button 
+                className='wrapper__headerGameModern_choiseBlock_firstItemModern' 
+                onClick={() => playGame('paper')}
+              >
+                <img src={paper}/>
+              </button>
+
+              <button 
+                className='wrapper__headerGameModern_choiseBlock_firstItemModern' 
+                onClick={() => playGame('scissors')}
+                >
+                  <img src={scissors}/>
+              </button>
+
+              <button 
+                className='wrapper__headerGameModern_choiseBlock_firstItemModern' 
+                onClick={() => playGame('rock')}
+              >
+                <img src={rock}/>
+              </button>
+
+              <button 
+                className='wrapper__headerGameModern_choiseBlock_firstItemModern' 
+                onClick={() => playGame('lizard')}
+              >
+                <img src={lizard}/>
+              </button>
+
+              <button 
+                className='wrapper__headerGameModern_choiseBlock_firstItemModern' 
+                onClick={() => playGame('spock')}
+              >
+                <img src={spock}/>
+
+              </button>
+            </div>
+          ) : (
+            <>
+            
+            </>
+          )}
+        </>
       ) : null}
 
-      <button className='wrapper__backToMenu' onClick={() => {
-        setMenu(true)
-        setUserScore(0)
-        setTies(0)
-        setCPUScore(0)
-      }}>HOME</button>
-      <button className='wrapper__rules' onClick={() => setOpenModalRules(true)}>RULES</button>
+      {modalBeforeGoToMenu === true && (
+        <div className='modalGoHome'>
+          <div className='modalGoHome__block'>
+            <p>DO YOU WANT SAVE CURRENT RESULT?</p>
+              <div className='modalGoHome__block_navBlock'>
+                <button onClick={() => {
+                  setMenu(true)
+                  setModalBeforeGoToMenu(false)
+                }}>YES</button>
+                <button onClick={() => {
+                  setMenu(true)
+                  setUserScore(0)
+                  setTies(0)
+                  setCPUScore(0)
+                  setModalBeforeGoToMenu(false)
+                }}>NO</button>
+              </div>
+            </div>
+          </div>
+        )}
 
-      {openModalRules === true && (
+      {menu === false && (
+        <>
+          <button className='wrapper__backToMenu' onClick={() => setModalBeforeGoToMenu(true)}>HOME</button>
+          <button className='wrapper__rules' onClick={() => {
+            if(mode === 'standard'){
+              setOpenModalRulesStandard(true)
+            }else if(mode === 'modern'){
+              setOpenModalRulesModern(true)
+            }
+          }}>RULES</button>
+        </>
+      )}
+
+      {openModalRulesStandard === true && (
         <div className='wrapper__modal'>
           <div className='wrapper__modal_rulesBlock'>
             <div className='wrapper__modal_rulesBlock_titleBlock'>
-              <p className='wrapper__modal_rulesBlock_titleBlock_title'>RULES</p>
-              <p className='wrapper__modal_rulesBlock_titleBlock_close' onClick={() => setOpenModalRules(false)}>X</p>
+              <p className='wrapper__modal_rulesBlock_titleBlock_title'>RULES STANDARD</p>
+              <p className='wrapper__modal_rulesBlock_titleBlock_close' onClick={() => setOpenModalRulesStandard(false)}>X</p>
             </div>
 
             <div className='wrapper__modal_rulesBlock_rulesBox'>
               <img src={rules}/>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {openModalRulesModern === true && (
+        <div className='wrapper__modal'>
+          <div className='wrapper__modal_rulesBlock'>
+            <div className='wrapper__modal_rulesBlock_titleBlock'>
+              <p className='wrapper__modal_rulesBlock_titleBlock_title'>RULES MODERN</p>
+              <p className='wrapper__modal_rulesBlock_titleBlock_close' onClick={() => setOpenModalRulesModern(false)}>X</p>
+            </div>
+
+            <div className='wrapper__modal_rulesBlock_rulesBox'>
+              <img className='wrapper__modal_rulesBlock_rulesBox_imgModern' src={rulesModern}/>
             </div>
           </div>
         </div>
